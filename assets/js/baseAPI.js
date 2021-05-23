4c710de79d61338554a9010ab19a31e03c124c16
shaonianren <2357247353@qq.com>
2021-05-23 20:35:54 +0800

    完成主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,14 @@ $(function () {
     $.ajaxPrefilter(function (option) {
         option.url = 'http://api-breakingnews-web.itheima.net' + option.url
         console.log(option);
+        if (option.url.indexOf('/my/') !== -1) {
+            option.headers = { Authorization: localStorage.getItem('token') }
+        }
+        option.complete =function(res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message ==='身份认证失败！') {
+                localStorage.removeItem('token');
+                location.href = '../../login.html'
+            }
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+    getUserInfo()
+    var layer = layui.layer
+    $('#loginout').on('click', function () {
+        layer.confirm('是否退出？', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token');
+            location.href = '../../login.html'
+            layer.close(index);
+        });
+    })
+    function getUserInfo() {
+        const layer = layui.layer
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                renderUserInfo(res)
+            }
+        })
+    }
+    function renderUserInfo(res) {
+        var uname = res.data.nickname || res.data.username;
+        $('.welcome').html('欢迎&nbsp;&nbsp;' + uname);
+        if (res.data.user_pic) {
+            $('.layui-nav-img').prop('src', res.data.user_pic).show().siblings('.text-avatar').hide();
+        } else {
+            $('.layui-nav-img').hide().siblings('.text-avatar').html(`${uname[0].toUpperCase()}`).show()
+        }
+    }
+})
